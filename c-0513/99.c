cd8424ecd5ea76e67f0dd9557683fdd1931af9b2
polyacetal <yotyna988@gmail.com>
2021-05-14 00:31:03 +0900

    bag fix

--- c-0513/99.c
+++ c-0513/99.c
@@ -8,14 +8,15 @@ int main(void)
 		printf("%dの段", n);
 		for (m = 1; m <= 9; m++) {
 			a = m * n;
-			if ( m == 9) {
-				printf(" %d\n", a);
-			} else if ( a / 10 == 0){
-				printf(" %d ", a);
+			if (a / 10 == 0){
+				printf("  %d", a);
+			} else if (m == 9){
+				printf(" %d", a);
 			} else {
 				printf(" %d", a);
 			}
 		}
+		printf("\n");
 	}
 
 		return(0);
